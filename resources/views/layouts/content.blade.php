3316ab891d2038717288ec315bbae2404dbbc0b4
周灿桢 <xdzcz@outlook.com>
2016-04-12 19:11:55 +0800

    首页字体修改

--- resources/views/layouts/content.blade.php
+++ resources/views/layouts/content.blade.php
@@ -12,19 +12,19 @@
 					
 					<div class="top-nav">
 						<ul class="res" > 
-							<li><a href="#" class="stfyt">服务</a></li>
-							<li><a href="#" class="stfyt">关于</a></li>
-							<li><a href="#" class="stfyt">联系我们</a></li>
+							<li><a href="#">服务</a></li>
+							<li><a href="#">关于</a></li>
+							<li><a href="#">联系我们</a></li>
 						</ul>
 					</div>
 				</div>
 
 				<div class="header-right">
 				    <div class="search">
-						<input class="stfyt" type="text" value="查找" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = '查找';}"/>
+						<input type="text" value="查找" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = '查找';}"/>
 						<input type="submit"  value=""/>		
 					</div>
-					<a href="/login" class="stfyt">登录</a>
+					<a href="/login">登录</a>
 				</div>
 
 				<div class="clearfix"> </div>
