2e9303d906c34249d1a5cdd83e491211084c9bb1
Alex <13301043@bjtu.edu.cn>
2016-04-20 16:11:24 +0800

    首页主页修改

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.content')
+@extends('layouts.content2')
 
 @section('title','首页')
 
@@ -40,37 +40,30 @@
 
 	<div class = "pictureSlide">
 		<div id="myCarousel" class="carousel slide">
-   <!-- 轮播（Carousel）指标 -->
-   <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-   </ol>   
-   <!-- 轮播（Carousel）项目 -->
-   <div class="carousel-inner">
-      <div class="item active">
-         <img src="/images/mo.jpg" alt="First slide">
-      </div>
-      <div class="item">
-         <img src="/images/b2.jpg" alt="Second slide">
-      </div>
-      <div class="item">
-         <img src="/images/b3.jpg" alt="Third slide">
-      </div>
-   </div>
-   <!-- 轮播（Carousel）导航 -->
-   <a class="carousel-control left" href="#myCarousel" 
-      data-slide="prev">&lsaquo;</a>
-   <a class="carousel-control right" href="#myCarousel" 
-      data-slide="next">&rsaquo;</a>
-</div> 
+		<!-- 轮播（Carousel）指标 -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>   
+		<!-- 轮播（Carousel）项目 -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="/images/img1.jpg" alt="First slide">
+			</div>
+			<div class="item">
+				<img src="/images/img2.jpg" alt="Second slide">
+			</div>
+			<div class="item">
+				<img src="/images/img3.jpg" alt="Third slide">
+			</div>
+		</div>
+		<!-- 轮播（Carousel）导航 -->
+		<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		</div> 
 
 
 	</div>
-
-
-
-
-
 
 @stop
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,7 +15,7 @@
 
 @section('content')
 
-    <div class="slider">
+    <!-- <div class="slider">
 	    <div class="callbacks_container">
 		    <ul class="rslides" id="slider">
 		        <li>
@@ -31,7 +31,35 @@
 		        </li>
 		    </ul>
 		</div>
-    </div>
+    </div> -->
+
+    <div class = "pictureSlide">
+		<div id="myCarousel" class="carousel slide">
+		<!-- 轮播（Carousel）指标 -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				<li data-target="#myCarousel" data-slide-to="1"></li>
+				<li data-target="#myCarousel" data-slide-to="2"></li>
+			</ol>   
+		<!-- 轮播（Carousel）项目 -->
+		<div class="carousel-inner">
+			<div class="item active">
+				<img src="/images/img1.jpg" alt="First slide">
+			</div>
+			<div class="item">
+				<img src="/images/img2.jpg" alt="Second slide">
+			</div>
+			<div class="item">
+				<img src="/images/img3.jpg" alt="Third slide">
+			</div>
+		</div>
+		<!-- 轮播（Carousel）导航 -->
+		<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		</div> 
+
+
+	</div>
 	<!--幻灯片区域结束-->
 
 	<div id="small-dialog5" class="mfp-hide"></div>
