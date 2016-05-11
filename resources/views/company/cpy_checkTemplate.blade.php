d47d86620cb725cffe700da4b7784a602500d0bd
周灿桢 <xdzcz@outlook.com>
2016-05-11 13:08:24 +0800

    基于css3 实现大图查看

--- resources/views/company/cpy_checkTemplate.blade.php
+++ resources/views/company/cpy_checkTemplate.blade.php
@@ -5,7 +5,8 @@
 
 @section('header')
 	@parent
-	<link rel="stylesheet" href="/css/company/cpy_checkTemplate.css">
+	<link rel="stylesheet" href="/css/company/cpy_checkTemplate/cpy_checkTemplate.css">
+	<link rel="stylesheet" href="/css/company/cpy_checkTemplate/showBigImg.css">
 @stop
 
 @section('footer')
@@ -15,12 +16,13 @@
 
 
 @section('sidebar-content')
+
 	<div ng-controller="checkTemplateCtrl" class="container-fluid">
 		<div class="row" id="mainbody">
 			<div class="thumbnail" ng-repeat="x in deployedImgs" on-finish-render-filters>
-				<a href="#">
-					<div class="imgs"">
-						<img ng-src="@{{ x.saving_path }}">
+				<a href="#@{{ x.template_name }}">
+					<div class="imgs">
+						<img id="img" ng-src="@{{ x.saving_path }}">
 					</div>
 				</a>
 				<div class="captain">
@@ -30,6 +32,14 @@
 					<div class="time">修改时间：@{{ x.updated_at }}</div>
 					<div class="author">作者：@{{ x.author_name }}</div>
 					<div class="author"><a href="" ng-click="deleteTemplate(x)">删除模板</a></div>
+				</div>
+				<div class="lb-overlay" id="@{{ x.template_name }}">
+					<a href="#" class="lb-close">x Close</a>
+					<img src="@{{ x.saving_path }}" alt="image01" />
+					<div>
+						<h3>pointe <span>/point/</h3>
+						<p>Dance performed on the tips of the toes</p>
+					</div>
 				</div>
 			</div>
 		</div>
