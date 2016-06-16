7b00aeca3edc1eddcbc5aa9cd8f3378aefbc89ac
周灿桢 <xdzcz@outlook.com>
2016-06-16 10:41:51 +0800

    实现公司首页新消息提醒时间计算

---
+++ public/js/layouts/cpy_sidebar/cpy_sidebar.js
@@ -0,0 +1,93 @@
+$(function() {
+
+    $('#side-menu').metisMenu();
+
+});
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function() {
+    $(window).bind("load resize", function() {
+        topOffset = 50;
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+
+    var url = window.location;
+    var element = $('ul.nav a').filter(function() {
+        return this.href == url || url.href.indexOf(this.href) == 0;
+    }).addClass('active').parent().parent().addClass('in').parent();
+    if (element.is('li')) {
+        element.addClass('active');
+    }
+});
+
+
+/**
+ * Created by Zhou Canzhen
+ * 2016/05/06
+ */
+yinjiApp.controller('cpyNavController',
+    function($scope,$http){
+        $scope.evaHour = "";
+        $scope.evaMinute = "";
+        $scope.evaSecond = "";
+        $scope.odHour = "";
+        $scope.odMinute = "";
+        $scope.odSecond = "";
+
+        var date = new Date(),
+            currentHour = date.getHours(),
+            currentMinute = date.getMinutes(),
+            currentSecond = date.getSeconds(),
+            latestOrderTime,latestEvaTime;
+
+
+        //获取首页订单
+        $http.get("/cpy/getEvaluations")
+        .success(function (response)
+        {
+            latestEvaTime = response[response.length-1].created_at.split(" ")[1].split(":");
+            $scope.evaHour = currentHour - latestEvaTime[0];
+            if ($scope.evaHour < 1){
+                $scope.evaMinute = currentMinute - latestEvaTime[1];
+                if ($scope.evaMinute < 1)
+                    $scope.evaSecond = currentSecond - latestEvaTime[2];
+            }
+            console.log($scope.evaHour+"\n"+$scope.evaMinute+"\n"+$scope.evaSecond);
+        });
+
+
+        //获取首页订单
+        $http.get("/cpy/getOrders")
+        .success(function (response)
+        {
+            latestOrderTime = response[response.length-1].order_date.split(" ")[1].split(":");
+            $scope.odHour = currentHour - latestOrderTime[0];
+            if ($scope.odHour < 1){
+                $scope.odMinute = currentMinute - latestOrderTime[1];
+                if ($scope.odMinute < 1)
+                    $scope.odSecond = currentSecond - latestOrderTime[2];
+            }
+            console.log($scope.odHour+"\n"+$scope.odMinute+"\n"+$scope.odSecond);
+        });
+
+
+        function getTimeInterval(date){
+            var dateDetail = date.split(":");
+            return dateDetail[0]*60+dateDetail[1];
+        }
+});
--- resources/views/layouts/cpy_sidebar.blade.php
+++ resources/views/layouts/cpy_sidebar.blade.php
@@ -3,22 +3,22 @@
 @section('header')
 	@parent
 	<!-- Custom CSS -->
-	<link href="/css/company/cpy_sidebar/cpy_sidebar.css" rel="stylesheet">
+	<link href="/css/layouts/cpy_sidebar/cpy_sidebar.css" rel="stylesheet">
 	<!-- Morris Charts CSS -->
-	<link href="/css/company/cpy_sidebar/morris.css" rel="stylesheet">
-	<link href="/css/company/cpy_sidebar/metisMenu.min.css" rel="stylesheet">
-	<link href="/css/company/cpy_sidebar/timeline.css" rel="stylesheet">
-	<link href="/css/company/cpy_sidebar/dataTables.bootstrap.css" rel="stylesheet">
-	<link href="/css/company/cpy_sidebar/responsive.dataTables.css" rel="stylesheet">
+	<link href="/css/layouts/cpy_sidebar/morris.css" rel="stylesheet">
+	<link href="/css/layouts/cpy_sidebar/metisMenu.min.css" rel="stylesheet">
+	<link href="/css/layouts/cpy_sidebar/timeline.css" rel="stylesheet">
+	<link href="/css/layouts/cpy_sidebar/dataTables.bootstrap.css" rel="stylesheet">
+	<link href="/css/layouts/cpy_sidebar/responsive.dataTables.css" rel="stylesheet">
 @stop
 
 @section('footer')
 	@parent
-	<script src="/js/company/cpy_sidebar/cpy_sidebar.js"></script>
+	<script src="/js/layouts/cpy_sidebar/cpy_sidebar.js"></script>
 	<!-- Morris Charts JavaScript -->
-	<script src="/js/company/cpy_sidebar/raphael.min.js"></script>
-	<script src="/js/company/cpy_sidebar/morris.min.js"></script>
-	<script src="/js/company/cpy_sidebar/metisMenu.min.js"></script>
+	<script src="/js/layouts/cpy_sidebar/raphael.min.js"></script>
+	<script src="/js/layouts/cpy_sidebar/morris.min.js"></script>
+	<script src="/js/layouts/cpy_sidebar/metisMenu.min.js"></script>
 @stop
 
 @section('body')
@@ -38,144 +38,7 @@
 			</div>
 			<!-- /.navbar-header -->
 
-			<ul class="nav navbar-top-links navbar-right">
-
-				<!--消息提醒-->
-				<!--
-				<li class="dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="fa fa-envelope fa-fw"></i>  <i class="fa fa-caret-down"></i>
-					</a>
-					<ul class="dropdown-menu dropdown-messages">
-						<li>
-							<a href="#">
-								<div>
-									<strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-								</div>
-								<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="#">
-								<div>
-									<strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-								</div>
-								<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="#">
-								<div>
-									<strong>John Smith</strong>
-                                    <span class="pull-right text-muted">
-                                        <em>Yesterday</em>
-                                    </span>
-								</div>
-								<div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eleifend...</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a class="text-center" href="#">
-								<strong>Read All Messages</strong>
-								<i class="fa fa-angle-right"></i>
-							</a>
-						</li>
-					</ul>
-				</li>
-				-->
-				<!--消息提醒结束-->
-
-				<!--任务完成情况-->
-				<!--
-				<li class="dropdown">
-					<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-						<i class="fa fa-tasks fa-fw"></i>  <i class="fa fa-caret-down"></i>
-					</a>
-					<ul class="dropdown-menu dropdown-tasks">
-						<li>
-							<a href="#">
-								<div>
-									<p>
-										<strong>Task 1</strong>
-										<span class="pull-right text-muted">40% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
-											<span class="sr-only">40% Complete (success)</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="#">
-								<div>
-									<p>
-										<strong>Task 2</strong>
-										<span class="pull-right text-muted">20% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
-											<span class="sr-only">20% Complete</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="#">
-								<div>
-									<p>
-										<strong>Task 3</strong>
-										<span class="pull-right text-muted">60% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
-											<span class="sr-only">60% Complete (warning)</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a href="#">
-								<div>
-									<p>
-										<strong>Task 4</strong>
-										<span class="pull-right text-muted">80% Complete</span>
-									</p>
-									<div class="progress progress-striped active">
-										<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-											<span class="sr-only">80% Complete (danger)</span>
-										</div>
-									</div>
-								</div>
-							</a>
-						</li>
-						<li class="divider"></li>
-						<li>
-							<a class="text-center" href="#">
-								<strong>See All Tasks</strong>
-								<i class="fa fa-angle-right"></i>
-							</a>
-						</li>
-					</ul>
-				</li>
-				-->
-				<!--任务完成情况结束-->
-
+			<ul class="nav navbar-top-links navbar-right" ng-controller="cpyNavController">
 
 				<?php
 				if (isset($_SESSION['ifLoggedIn']) && $_SESSION['ifLoggedIn'] == "y"){
@@ -190,7 +53,12 @@
 							<a href="/cpy_checkEvaluation">
 								<div>
 									<i class="fa fa-comment fa-fw"></i> 新评价
-									<span class="pull-right text-muted small">4 minutes ago</span>
+									<span class="pull-right text-muted small">
+										<span ng-show="evaHour != ''">@{{ evaHour }} 小时</span>
+										<span ng-show="evaMinute != ''">@{{ evaMinute }} 分钟</span>
+										<span ng-show="evaSecond != ''">@{{ evaSecond }} 秒</span>
+										前
+									</span>
 								</div>
 							</a>
 						</li>
@@ -199,7 +67,12 @@
 							<a href="/cpy_checkOrder">
 								<div>
 									<i class="fa fa-edit fa-fw"></i> 新订单
-									<span class="pull-right text-muted small">12 minutes ago</span>
+									<span class="pull-right text-muted small">
+										<span ng-show="odHour != ''">@{{ odHour }} 小时</span>
+										<span ng-show="odMinute != ''">@{{ odMinute }} 分钟</span>
+										<span ng-show="odSecond != ''">@{{ odSecond }} 秒</span>
+										前
+									</span>
 								</div>
 							</a>
 						</li>
