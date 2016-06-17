7729ea9659a8b7b9cd9f2ef85563eb3fdb88c274
xx502003587 <13301043@bjtu.edu.cn>
2016-06-17 15:56:58 +0800

    无

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -55,9 +55,7 @@
 
 @section('dif')
 	<!--$_SESSION['ifLoggedIn'] == 'y'-->
-		@if(isset($_SESSION['ifLoggedIn'])&&$_SESSION['ifLoggedIn']=='y')
-			
-		
+		@if(isset($_SESSION['ifLoggedIn'])&&$_SESSION['ifLoggedIn']=='y')	
 			<div class="top-nav">
 						<ul class="res" > 
 							<li class = "topLine"><a href="#">纪念册</a>
